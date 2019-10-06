0b882d7c3ef502d1f3e9000bf7237a1d08d45df5
Roy Davis <myroy1024@yahoo.com>
2019-10-06 17:16:57 -0400

    set the variables and basic html and css

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+// Assigning variable
+let pinkcrys = Math.floor(Math.random() * (12 - 1)) +1;
+let greencrys = Math.floor(Math.random() * (12 - 1)) +1;
+let yellowcrys = Math.floor(Math.random() * (12 - 1)) +1;
+let purplecrys = Math.floor(Math.random() * (12 - 1)) +1;
+let randomNum = Math.floor(Math.random() * (120 - 19 ));
+console.log(pinkcrys);
+console.log(greencrys);
+console.log(yellowcrys);
+console.log(purplecrys);
+console.log(randomNum);
+
+let userScore = 0;
+let wins = 0;
+let losses = 0;
+
+
+
+
+
+
+
+
+
+
